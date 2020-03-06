479f3b9bfe614a27bb5a1e0ca9b4a21ab8bd691e
XenoAmess <xenoamess@gmail.com>
2020-03-06 14:35:58 +0800

    annotator

---
+++ src/main/gen/com/xenoamess/x8l/idea_plugin/psi/X8lCommentNodeContent.java
@@ -0,0 +1,10 @@
+// This is a generated file. Not intended for manual editing.
+package com.xenoamess.x8l.idea_plugin.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface X8lCommentNodeContent extends PsiElement {
+
+}
