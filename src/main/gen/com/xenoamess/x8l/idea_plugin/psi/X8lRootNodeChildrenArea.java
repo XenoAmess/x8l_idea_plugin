d873ac84fe83d32c1656b56bcdb7927b5e8e6958
XenoAmess <xenoamess@gmail.com>
2020-03-08 06:38:19 +0800

    update commenter

---
+++ src/main/gen/com/xenoamess/x8l/idea_plugin/psi/X8lRootNodeChildrenArea.java
@@ -0,0 +1,13 @@
+// This is a generated file. Not intended for manual editing.
+package com.xenoamess.x8l.idea_plugin.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface X8lRootNodeChildrenArea extends PsiElement {
+
+  @NotNull
+  X8lContentNodeChildrenArea getContentNodeChildrenArea();
+
+}
